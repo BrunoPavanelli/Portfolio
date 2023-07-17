a5502418e903e0748f4998fb34f54c8bf8e4b6a5
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-07-17 10:09:57 -0300

    feat: change some global colors

--- src/styles/Global.tsx
+++ src/styles/Global.tsx
@@ -84,8 +84,8 @@ export const GlobalStyle = ({ children }: GlobalStyleProps): JSX.Element => {
 
   const theme = createTheme({
     colors: {
-      brand1: "#623CEA",
-      brand2: "#311E75",
+      brand1: "#2488a1",
+      brand2: "#4ed3eb",
 
       grey0: "#0A0A0B",
       grey1: "#121214",

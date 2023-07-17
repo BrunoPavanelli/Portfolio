a4a583785efdb8b6f5920521353e0b247b50525e
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-07-17 13:06:14 -0300

    fix: remove portfolio project card

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -29,19 +29,19 @@ export const Project = (): JSX.Element => {
       const dataRepos = await fetch(
         `https://api.github.com/users/${userData.githubUser}/repos?sort=created`
       );
-
       const dataCosmosSearch = await fetch(
         `https://api.github.com/orgs/CosmosSearch-Grupo-4-Kenzie-Academy-M3/repos`
-      )
-
+        )
       const dataBiblioteka = await fetch(
         `https://api.github.com/orgs/m5-t15-g23/repos`
-      )
+        )
+          
       const jsonRepos = await dataRepos.json();
       const jsonCosmosSearch = await dataCosmosSearch.json(); 
       const jsonBiblioteka = await dataBiblioteka.json();
-
-      setRepositories([...jsonRepos, jsonCosmosSearch[0], ...jsonBiblioteka]);
+      const jsonReposFilter = jsonRepos.filter((repo: ReposType) => repo.name !== "Portfolio")
+      
+      setRepositories([...jsonReposFilter, jsonCosmosSearch[0], ...jsonBiblioteka]);
 
       return jsonRepos;
     };

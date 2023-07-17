c6919f4ed1bddb5ceb6e35cafe136d2fecde6129
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2023-07-17 15:10:10 -0300

    feat: implement language option persistence with local storage

--- src/providers/LanguageProvider/index.tsx
+++ src/providers/LanguageProvider/index.tsx
@@ -1,5 +1,5 @@
 import { ILanguage, enUSA, ptBr } from "@/utils/languageData";
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 
 interface ILanguageContext {
     language: ILanguage,
@@ -13,10 +13,17 @@ interface IChildren {
 export const LanguageContext = createContext<ILanguageContext>({} as ILanguageContext)
 
 export const LanguageProvider = ({children}: IChildren) => {
+
+    useEffect(() => {
+        const languageLS = localStorage.getItem("@Portfolio:Language")
+        if (languageLS) languageLS === "enUSA" ? setLanguage(ptBr) : setLanguage(enUSA)
+    }, [])
+    
     const [language, setLanguage] = useState<ILanguage>(enUSA)
 
     const changeLanguage = () => {
         language === enUSA ? setLanguage(ptBr) : setLanguage(enUSA)
+        language === enUSA ? localStorage.setItem("@Portfolio:Language", "enUSA") : localStorage.setItem("@Portfolio:Language", "ptBR")
     }
 
     return (
